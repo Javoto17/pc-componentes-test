aff55652cf2c344cc7c01e74985d72cefdce6545
Javoto <javi.lopez_@outlook.com>
2020-09-26 17:07:23 +0200

    Fix https

--- src/api/products.ts
+++ src/api/products.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const getProducts = async (name: string) => {
   try {
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${name}`);
+    const res = await axios.get(
+      `https://api.tvmaze.com/search/shows?q=${name}`
+    );
     console.log(res);
     return res;
   } catch (error) {
